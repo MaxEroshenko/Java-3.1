144e357144b55f9c62b8372a8c1582d1cf2320cc
Maxim Eroshenko <eroshenko.maksim.87@mail.ru>
2020-07-21 20:24:13 +0300

    Создание кода расчета миль

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        float cost = 10500;
+        float one_mile = 20;
+        float bonus = cost / one_mile;
+        System.out.println(bonus);
+    }
+}
