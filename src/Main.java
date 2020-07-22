357681ae272778cad6d4f20f144b13ccba17ae95
Maxim Eroshenko <eroshenko.maksim.87@mail.ru>
2020-07-22 20:32:37 +0300

    Исправление имени (oneMile) переменной в коде

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         float cost = 10500;
-        float one_mile = 20;
-        float bonus = cost / one_mile;
+        float oneMile = 20;
+        float bonus = cost / oneMile;
         System.out.println(bonus);
     }
 }
